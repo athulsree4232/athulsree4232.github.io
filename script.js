0b13fb8b35d499ec15b02fe53ac0e1d6325aa546
athulkrishna <athulkrishna4232@gmail.com>
2021-06-11 22:59:52 +0530

    update

--- script.js
+++ script.js
@@ -31,9 +31,17 @@ $(document).ready(function(){
             }
         },
 
-        success:function(submit){
+        
 
-            
+    
+    })
+    
+
+    
+
+    $("#contact-form").submit((e)=>{
+        e.preventDefault()
+       
         $.ajax({
             url:"https://script.google.com/macros/s/AKfycbwpc2tfmDRo5QHce3JB6EME09CKjzz6m-nH_yHUotEV4R4B-Cq7aJzmxFWcEWvuuiK5Ug/exec",
             data:$("#contact-form").serialize(),
@@ -48,34 +56,8 @@ $(document).ready(function(){
   
             }
         })
-
-        }
-
     
     })
-    
-
-    
-
-    // $("#contact-form").submit((e)=>{
-    //     e.preventDefault()
-       
-    //     $.ajax({
-    //         url:"https://script.google.com/macros/s/AKfycbwpc2tfmDRo5QHce3JB6EME09CKjzz6m-nH_yHUotEV4R4B-Cq7aJzmxFWcEWvuuiK5Ug/exec",
-    //         data:$("#contact-form").serialize(),
-    //         method:"post",
-    //         success:function (response){
-    //             alert("Form submitted successfully")
-    //             window.location.reload()
-    //             //window.location.href="https://google.com"
-    //         },
-    //         error:function (err){
-    //             alert("Something Error")
-  
-    //         }
-    //     })
-    
-    // })
 
 
 })
